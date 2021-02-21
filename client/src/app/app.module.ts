13e2eab84c5f6c12aaa6bada02977300aef0988c
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-02-21 08:21:42 -0500

    add home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
