0c1b1e1b2c3be837aac6a308972fd37798986374
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-02-24 08:07:18 -0500

    profile update in client app

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Injectable } from '@angular/core'
 import { environment } from 'src/environments/environment'
-import { Member } from '../_models/member';
+import { Member } from '../_models/member'
 
 @Injectable({
   providedIn: 'root'
@@ -31,5 +31,16 @@ export class MembersService {
    */
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'users/' + username)
+  }
+
+  /**
+   * Updates the API with new user information
+   *
+   * @param {Member} member
+   * @return {*} 
+   * @memberof MembersService
+   */
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member)
   }
 }
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,11 +1,11 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { take } from 'rxjs/operators';
-import { Member } from 'src/app/_models/member';
-import { User } from 'src/app/_models/user';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
+import { ToastrService } from 'ngx-toastr'
+import { take } from 'rxjs/operators'
+import { Member } from 'src/app/_models/member'
+import { User } from 'src/app/_models/user'
 import { AccountService } from 'src/app/_services/account.service'
-import { MembersService } from 'src/app/_services/members.service';
+import { MembersService } from 'src/app/_services/members.service'
 
 @Component({
   selector: 'app-member-edit',
@@ -39,9 +39,10 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    // console.log(this.member)
-    this.toastr.success("Profile updated")
-    this.editForm.reset(this.member)
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success("Profile updated")
+      this.editForm.reset(this.member)
+    })
   }
 }
  
