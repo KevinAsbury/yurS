3f91c1d1b5fac6f6fea0953443060bbf8dbee51f
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-02-25 17:09:44 -0500

    custom validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -6,7 +6,7 @@ import {
   Output,
   ɵɵsetComponentScope,
 } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { AbstractControl, ControlContainer, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms'
 import { ToastrService } from 'ngx-toastr'
 import { AccountService } from '../_services/account.service'
 
@@ -31,10 +31,30 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl(),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password'), Validators.minLength(4), Validators.maxLength(20)]),
     })
+
+    // Check password field for changes and make confirmPassword field aware of change
+    // just in case someone changes the password field after confirming password match
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity()
+    })
+  }
+
+  
+  /**
+   * Compare confirm password control with password you want to match to
+   * if passwords match return null meaning validation passed else return isMatching true
+   * 
+   * @param {string} matchTo
+   * @return {*}  {ValidatorFn}
+   */
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : { isMatching: true }
+    }
   }
 
   /**
