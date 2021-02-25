6e43b76ea5316e620e38768e0da3e6bcd6af6594
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-02-25 17:54:40 -0500

    form buider

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -6,7 +6,7 @@ import {
   Output,
   ɵɵsetComponentScope,
 } from '@angular/core'
-import { AbstractControl, ControlContainer, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms'
+import { AbstractControl, ControlContainer, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms'
 import { ToastrService } from 'ngx-toastr'
 import { AccountService } from '../_services/account.service'
 
@@ -23,6 +23,7 @@ export class RegisterComponent implements OnInit {
   constructor(
     private accountService: AccountService,
     private toastr: ToastrService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -30,10 +31,10 @@ export class RegisterComponent implements OnInit {
   }
 
   initializeForm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password'), Validators.minLength(4), Validators.maxLength(20)]),
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password'), Validators.minLength(4), Validators.maxLength(20)]],
     })
 
     // Check password field for changes and make confirmPassword field aware of change
