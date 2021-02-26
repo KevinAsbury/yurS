5338e62e025a0ebdea4844095fb28acc65773ed7
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-02-26 15:40:14 -0500

    added gender filtering to api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -36,7 +36,16 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
+            var user = await _repo.GetByUsernameAsync(User.GetUserName());
+            userParams.CurrentUsername = user.UserName;
+
+            if(string.IsNullOrEmpty(userParams.Gender))
+            {
+                // userParams.Gender = user.Gender == "male" ? "female" : "male";
+            }
+
             var users = await _repo.GetMembersAsync(userParams);
+
             Response.AddPaginationHeader(users.CurrentPage, users.PageSize, 
                 users.TotalCount, users.TotalPages);
 
