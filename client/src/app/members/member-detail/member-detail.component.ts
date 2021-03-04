db240ad20aa1392acf556bb6a34691a4019ae42e
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-03-04 11:56:16 -0500

    added activation for messages tab

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Member } from 'src/app/_models/member'
 import { MembersService } from 'src/app/_services/members.service'
 import { NgxGalleryOptions } from '@kolkov/ngx-gallery'
 import { NgxGalleryImage } from '@kolkov/ngx-gallery'
 import { NgxGalleryAnimation } from '@kolkov/ngx-gallery'
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs'
+import { MessageService } from 'src/app/_services/message.service'
+import { Message } from 'src/app/_models/message'
 
 @Component({
   selector: 'app-member-detail',
@@ -12,13 +15,17 @@ import { NgxGalleryAnimation } from '@kolkov/ngx-gallery'
   styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs') memberTabs: TabsetComponent
   member: Member
   galleryOptions: NgxGalleryOptions[]
   galleryImages: NgxGalleryImage[]
+  activeTab: TabDirective
+  messages: Message[] = []
 
   constructor(
     private memberService: MembersService,
     private route: ActivatedRoute,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -67,5 +74,18 @@ export class MemberDetailComponent implements OnInit {
         this.member = member
         this.galleryImages = this.getImages()
       })
+  }
+
+  loadMessages() {
+    this.messageService.getMessageThread(this.member.userName).subscribe(messages => {
+      this.messages = messages
+    })
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data
+    if (this.activeTab.heading === 'Messages' && this.messages.length === 0) {
+      this.loadMessages()
+    }
   }
 }
